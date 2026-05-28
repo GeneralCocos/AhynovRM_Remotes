ad50fd6eae63f29dcce82c4f833f0a37017c01ec
GeneralCocos <vl@d-sotskov.ru>
2026-05-28 21:59:22 +0300

    add new feature

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// Ахунов Руслан Маратович Ам-1-25
 //solve task2
+internal class Program
+{
+    static public double Sqr(double X)
+    {
+        return X * X;
+    }
+
+    static void Main(string[] args)
+    {
+        //solve task2
+
+        Console.WriteLine(Sqr(5));
+    }
+}
